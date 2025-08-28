69ce5795bdb671b9a57b464b55aa9871f9f78e49
Hamzah Saeed <muhammad.hamzah.saeed@gmail.com>
2025-08-28 13:41:53 -0400

    Added a new filter, fixed up some minor details

--- filters.py
+++ filters.py
@@ -9,3 +9,14 @@ def filter_grayscale(frame):
 
 def filter_canny(frame, low=80, high=160):
     return cv.Canny(frame, low, high)
+
+def filter_blur(frame):
+    return cv.blur(frame, (20,20))
+
+def filter_cartoon(frame):
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    gray = cv.medianBlur(gray, 5)
+    edges = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C,
+                                 cv.THRESH_BINARY, 9, 9)
+    color = cv.bilateralFilter(frame, 9, 300, 300)
+    return cv.bitwise_and(color, color, mask=edges)
--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import cv2 as cv
 import filters
 
 capture = cv.VideoCapture(0)
+capture.set(cv.CAP_PROP_FRAME_WIDTH, 1920)
+capture.set(cv.CAP_PROP_FRAME_HEIGHT, 1080)
 lastKey = 1
+
+
 if not capture.isOpened(): 
     print("Camera cannot be opened")
     exit()
@@ -13,14 +17,26 @@ while True:
         print("The frame wasn't read.")
         break
 
-    #Checks for last keystroke to determine which filter to use. System is somewhat janky, might have to spam the key, could be refined
-    if cv.waitKey(1) == ord('3'):
-        lastKey = 3
-    elif cv.waitKey(1) == ord('2'):
-        lastKey = 2
-    elif cv.waitKey(1) == ord('1'):
+    #Checks for last keystroke to determine which filter to use. System is somewhat janky, might have to spam the key
+    key = cv.waitKey(1) & 0xFF
+    if key == ord('1'):
         lastKey = 1
-    if lastKey == 3:
+    elif key == ord('2'):
+        lastKey = 2
+    elif key == ord('3'):
+        lastKey = 3
+    elif key == ord('4'):
+        lastKey = 4
+    elif key == ord('5'):
+        lastKey = 5
+    elif key == ord('q'):
+        break
+
+    if lastKey == 5: 
+        customFrame = filters.filter_cartoon(frame)
+    elif lastKey == 4: 
+        customFrame = filters.filter_blur(frame)
+    elif lastKey == 3:
         customFrame = filters.filter_canny(frame)
     elif lastKey == 2:
         customFrame = filters.filter_grayscale(frame)
